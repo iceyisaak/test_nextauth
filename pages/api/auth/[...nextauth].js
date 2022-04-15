119b95a2b67b9da1548c5418f3da44c99f21879d
iceyisaak <me@iceyisaak.com>
2022-04-15 20:20:59 +0200

    Resolving CSRF Error

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,13 @@
 import NextAuth from "next-auth";
-import EmailProvider from "next-auth/providers/email";
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+// import EmailProvider from "next-auth/providers/email";
+// import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import GitHubProvider from "next-auth/providers/github";
 // import TwitterProvider from "next-auth/providers/twitter";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 import Auth0Provider from "next-auth/providers/auth0";
 import LinkedInProvider from "next-auth/providers/linkedin";
-import clientPromise from "../lib/mongodb";
+// import clientPromise from "../lib/mongodb";
 import CredentialsProvider from "next-auth/providers/credentials";
 import connectDB from "../lib/connectDB";
 import bcrypt from 'bcrypt';
