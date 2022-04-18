cdd468857faea39ee1fead7f17826050c8d874af
iceyisaak <me@iceyisaak.com>
2022-04-18 16:40:16 +0200

    Clean up the nextauth file

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,9 @@
 import NextAuth from "next-auth";
-// import EmailProvider from "next-auth/providers/email";
-// import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import GitHubProvider from "next-auth/providers/github";
-// import TwitterProvider from "next-auth/providers/twitter";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 import Auth0Provider from "next-auth/providers/auth0";
 import LinkedInProvider from "next-auth/providers/linkedin";
-// import clientPromise from "../lib/mongodb";
 import CredentialsProvider from "next-auth/providers/credentials";
 import connectDB from "../lib/connectDB";
 import bcrypt from 'bcrypt';
@@ -18,20 +14,8 @@ connectDB();
 
 export default NextAuth({
 
-  // adapter: MongoDBAdapter(clientPromise),
   providers: [
     // Sign in With Email
-    // EmailProvider({
-    //   server: {
-    //     host: process.env.NEXT_PUBLIC_EMAIL_SERVER_HOST,
-    //     port: process.env.NEXT_PUBLIC_EMAIL_SERVER_PORT,
-    //     auth: {
-    //       user: process.env.NEXT_PUBLIC_EMAIL_SERVER_USER,
-    //       pass: process.env.NEXT_PUBLIC_EMAIL_SERVER_PASSWORD
-    //     }
-    //   },
-    //   from: process.env.NEXT_PUBLIC_EMAIL_FROM
-    // }),
     CredentialsProvider({
       // The name to display on the sign in form (e.g. "Sign in with...")
       name: "Credentials",
